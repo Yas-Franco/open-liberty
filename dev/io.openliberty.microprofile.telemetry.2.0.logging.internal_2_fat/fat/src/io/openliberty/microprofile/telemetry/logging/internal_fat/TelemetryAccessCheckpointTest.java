abb822fc12b0cbc57709f7c2ac85255c04184049
yasmin.aumeeruddy <yasmin.aumeeruddy@ibm.com>
2025-06-13 19:50:14 +0100

    Add class rules to Telemetry logging tests

--- dev/io.openliberty.microprofile.telemetry.2.0.logging.internal_2_fat/fat/src/io/openliberty/microprofile/telemetry/logging/internal_fat/TelemetryAccessCheckpointTest.java
+++ dev/io.openliberty.microprofile.telemetry.2.0.logging.internal_2_fat/fat/src/io/openliberty/microprofile/telemetry/logging/internal_fat/TelemetryAccessCheckpointTest.java
@@ -29,6 +29,7 @@ import componenttest.annotation.Server;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.rules.repeater.CheckpointRule;
 import componenttest.rules.repeater.CheckpointRule.ServerMode;
+import org.junit.rules.RuleChain;
 import componenttest.rules.repeater.RepeatTests;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.utils.FATServletClient;
@@ -54,6 +55,9 @@ public class TelemetryAccessCheckpointTest extends FATServletClient {
     // Will re-enable in follow-on issue.
     @ClassRule
     public static RepeatTests rt = TelemetryActions.telemetry21andLatest20Repeats(SERVER_NAME);
+
+    @ClassRule
+    public static RuleChain chain = RuleChain.outerRule(rt).around(checkpointRule);
 
     @Server(SERVER_NAME)
     public static LibertyServer server;
