b74c8388453bdce952dca5739ece97f9b0f6dab8
Phu Dinh <pmdinh@us.ibm.com>
2025-12-10 13:35:58 -0500

    Cleanup NettyVirtualConnectionImpl

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/NettyVirtualConnectionImpl.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/NettyVirtualConnectionImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 IBM Corporation and others.
+ * Copyright (c) 2023, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -15,11 +15,7 @@ import java.util.Map;
 import com.ibm.wsspi.channelfw.ConnectionDescriptor;
 import com.ibm.wsspi.channelfw.VirtualConnection;
 
-/**
- *
- */
 public class NettyVirtualConnectionImpl implements VirtualConnection {
-
     public static final NettyVirtualConnectionImpl DUMMY_NETTY_VC;
 
     static {
@@ -28,6 +24,8 @@ public class NettyVirtualConnectionImpl implements VirtualConnection {
     }
 
     private Map<Object, Object> stateStore = null;
+    private boolean inetAddressingValid = true;
+    private ConnectionDescriptor connDesc = null;
 
     protected NettyVirtualConnectionImpl() {
     }
@@ -44,140 +42,111 @@ public class NettyVirtualConnectionImpl implements VirtualConnection {
 
     @Override
     public void destroy() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public Map<Object, Object> getStateMap() {
-        // TODO Auto-generated method stub
         return stateStore;
     }
 
     @Override
     public boolean requestPermissionToRead() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean requestPermissionToWrite() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean requestPermissionToClose(long waitForPermission) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public void setReadStateToDone() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setWriteStateToDone() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public boolean isInputStateTrackingOperational() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public Object getLockObject() {
-        // TODO Auto-generated method stub
         return this;
     }
 
     @Override
     public boolean requestPermissionToFinishRead() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean requestPermissionToFinishWrite() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public void setReadStatetoCloseAllowedNoSync() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setWriteStatetoCloseAllowedNoSync() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public boolean getCloseWaiting() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isCloseWithReadOutstanding() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isCloseWithWriteOutstanding() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public void setInetAddressingValid(boolean _newValue) {
-        // TODO Auto-generated method stub
-
+        this.inetAddressingValid = _newValue;
     }
 
     @Override
     public boolean getInetAddressingValid() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.inetAddressingValid;
     }
 
     @Override
     public void setConnectionDescriptor(ConnectionDescriptor _newObject) {
-        // TODO Auto-generated method stub
-
+        this.connDesc = _newObject;
     }
 
     @Override
     public ConnectionDescriptor getConnectionDescriptor() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.connDesc;
     }
 
     @Override
     public int attemptToSetFileChannelCapable(int value) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int getFileChannelCapable() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public boolean isFileChannelCapable() {
-        // TODO Auto-generated method stub
         return false;
     }
-
 }
