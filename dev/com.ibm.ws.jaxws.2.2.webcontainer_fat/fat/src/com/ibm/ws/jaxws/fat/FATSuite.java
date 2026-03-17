5d9f7a88b4fd8368889ef265c2cd74ed61b479c2
Paul Nicolucci <pnicolucci@gmail.com>
2026-03-17 07:19:19 -0400

    Repeat jaxws FATs for EE11
    Strip _EE11_FEATURES from testmethod update SkipForRepeat for EE11
    
    Remove new EmptyAction()

--- dev/com.ibm.ws.jaxws.2.2.webcontainer_fat/fat/src/com/ibm/ws/jaxws/fat/FATSuite.java
+++ dev/com.ibm.ws.jaxws.2.2.webcontainer_fat/fat/src/com/ibm/ws/jaxws/fat/FATSuite.java
@@ -14,7 +14,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import componenttest.rules.repeater.EmptyAction;
 import componenttest.rules.repeater.FeatureReplacementAction;
 import componenttest.rules.repeater.RepeatTests;
 
@@ -48,6 +47,22 @@ import componenttest.rules.repeater.RepeatTests;
 })
 public class FATSuite {
 
+    /*@formatter:off*/
     @ClassRule
-    public static RepeatTests r = RepeatTests.with(new EmptyAction().fullFATOnly()).andWith(FeatureReplacementAction.EE9_FEATURES().removeFeature("jaxwstest-2.2").addFeature("xmlwstest-3.0").conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11)).andWith(FeatureReplacementAction.EE10_FEATURES().removeFeature("jaxwstest-2.2").removeFeature("xmlwstest-3.0").addFeature("xmlwstest-4.0"));
+    public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.NO_REPLACEMENT()
+                                                    .fullFATOnly())
+                                    .andWith(FeatureReplacementAction.EE9_FEATURES()
+                                                    .removeFeature("jaxwstest-2.2")
+                                                    .addFeature("xmlwstest-3.0")
+                                                    .conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
+                                    .andWith(FeatureReplacementAction.EE10_FEATURES()
+                                                    .removeFeature("jaxwstest-2.2")
+                                                    .removeFeature("xmlwstest-3.0")
+                                                    .addFeature("xmlwstest-4.0")
+                                                    .conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+                                    .andWith(FeatureReplacementAction.EE11_FEATURES()
+                                                    .removeFeature("jaxwstest-2.2")
+                                                    .removeFeature("xmlwstest-3.0")
+                                                    .addFeature("xmlwstest-4.0"));
+    /*@formatter:on*/
 }
