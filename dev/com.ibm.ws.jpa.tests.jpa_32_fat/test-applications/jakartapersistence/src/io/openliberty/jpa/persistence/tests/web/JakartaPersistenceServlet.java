7872a0f7afce7ed1fd2c362f4c4b4a6a0e16d505
pardhiv-krishna <pardhiv.krishna@ibm.com>
2026-04-24 11:08:01 +0530

    test method

--- dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
@@ -1911,6 +1911,41 @@ public class JakartaPersistenceServlet extends FATServlet {
         }
     }
 
+    @Test
+    public void testConstructorExpressionWithCasePrimitiveLong() throws Exception {
+        deleteAllEntities(SimpleEmployee.class);
+
+        SimpleEmployee emp1 = new SimpleEmployee("Tony", 35000L);
+        SimpleEmployee emp2 = new SimpleEmployee("Chris", 75000L);
+
+        tx.begin();
+        em.persist(emp1);
+        em.persist(emp2);
+        tx.commit();
+
+        try {
+            tx.begin();
+            EmployeeSalaryDTO result1 = em.createQuery(
+                                    "SELECT NEW io.openliberty.jpa.persistence.tests.models.EmployeeSalaryDTO(" +
+                                    "e.salary, " +
+                                    "CASE WHEN e.salary > 50000 THEN e.salary ELSE 0 END" +
+                                    ") FROM SimpleEmployee e WHERE e.name = :name", EmployeeSalaryDTO.class)
+                                    .setParameter("name", "Tony")
+                                    .getSingleResult();
+
+            assertNotNull(result1);
+            assertEquals(35000L, result1.salary());
+            assertEquals("Adjusted salary should be 0 (below threshold)", 0L, result1.adjustedSalary());
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.getStatus() == jakarta.transaction.Status.STATUS_ACTIVE) {
+                tx.rollback();
+            }
+            throw e;
+        }
+    }
+
     /**
      * Utility method to drop all entities from table.
      *
